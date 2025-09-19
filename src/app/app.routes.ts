c832dd8b09d3cd45ba2a53db507a3ed391232107
aixa <aixa.gonzalesdelvalle.aburto@vallegrande.edu.pe>
2025-09-19 01:44:49 -0500

    add nosotros and services

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Product } from './pages/product/product';
 import { Services } from './pages/services/services';
+import { Nosotros } from './pages/nosotros/nosotros';
 
 
 export const routes: Routes = [
@@ -12,6 +13,13 @@ export const routes: Routes = [
         path: 'services',
         component: Services
     },
+
+    {
+        path: 'nosotros',
+        component: Nosotros
+    },
+
+
     {
         path: '',
         pathMatch: 'full',
